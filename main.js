3093aa58b8359607b13d7f170fe8237fd6095d65
Steven Brooks Mogbo <stevenmogbo@yahoo.com>
2020-09-22 23:53:56 -0400

    fixed prompt and roles

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ var connection = mysql.createConnection({
 
   // Your password
   password: "root",
-  database: "employee_maker"
+  database: "employee_trackerDB"
 });
 connection.connect(function (err) {
   if (err) throw err;
@@ -24,7 +24,17 @@ connection.connect(function (err) {
 });
 
 function promptUser() {
-  const options = ["View All Employees", "View All Employees by Department", "View All Employees by Managemnt", "Add employee", "Remove employee", "Update Employee Role", "Update Employee Manager", "exit"];
+  const options = ["View All Employees",//0
+  "View All Employees by Department", //1
+  "View All Employees by Managemnt", //2
+  "Add employee", //3
+  "Remove employee", //4
+  "Update Employee Role", //5
+  "Update Employee Manager", //6
+  "view all roles",//7
+  "add a department",//8
+  "add a role",//9
+  "exit"];//9
   inquirer.prompt([
     {
       type: "list",
@@ -36,18 +46,24 @@ function promptUser() {
     if (response.choice == options[0]) {
       allEmployees();
     } else if (response.choice == options[1]) {
-      //getTop5000Artists();
+      ViewAllEmployeesDepartment();
     } else if (response.choice == options[2]) {
-      //getDataInRange();
+      employeesbyManagemnt();
     } else if (response.choice == options[3]) {
-      // getSongByName();
+      addEmployee();
     } else if (response.choice == options[4]) {
-      getSongByName();
+      deleteEmployee();
     } else if (response.choice == options[5]) {
-      //getSongByName();
+      updateRole();
     } else if (response.choice == options[6]) {
-      //getSongByName();
+      updateEmployeeManager();
     } else if (response.choice == options[7]) {
+      viewAllRoles();
+    }else if (response.choice == options[8]) {
+      addDepartment();
+    }else if (response.choice == options[9]) {
+      addRole();
+    }else if (response.choice == options[10]) {
       connection.end();
     }
   })
@@ -72,4 +88,319 @@ function allEmployees() {
     }
   });
 }
+//View All Employees by department
+function ViewAllEmployeesDepartment() {
+  connection.query("select * from department", (error, listofDepartments) => {
+    if (error) {
+      console.log(error);
+      connection.end();
+    } else {
+      const departmentNames = listofDepartments.map((department) => department.name);
+      inquirer.prompt([
+        // prompt user for department to view
+        {
+          type: "list",
+          message: "Which department would you like to view?",
+          choices: departmentNames,
+          name: "departmentName"
+        },
+      ]).then((response) => {
+        // JOIN employee, role, AND department TABLES TO COMBINE DATA AND RETURN employee ID, first and last names, and title DATA for the department chosen by user
+        connection.query(`select employee.id, employee.first_name, employee.last_name, role.title from employee 
+                          inner join role on role.id = employee.role_id 
+                          inner join department on department.id = role.department_id
+                        WHERE ?`, { name: response.departmentName }, (error, results) => {
+          console.log("\n");
+          // DISPLAY results for user
+          console.table(results);
+          console.log("\n");
+          promptUser();
+        });
+      });
+    }
+  });
+}
+function employeesbyManagemnt() {
+  connection.query("select * from role", (error, listofDepartments) => {
+    if (error) {
+      console.log(error);
+      connection.end();
+    } else {
+      const departmentNames = listofDepartments.map((role) => role.name);
+      inquirer.prompt([
+        // prompt user for department to view
+        {
+          type: "list",
+          message: "Which manager would you like to view?",
+          choices: managerName,
+          name: "managerName"
+        },
+      ]).then((response) => {
+        // JOIN employee, role, AND department TABLES TO COMBINE DATA AND RETURN employee ID, first and last names, and title DATA for the department chosen by user
+        connection.query(`select employee.id, employee.first_name, employee.last_name, role.title from employee 
+                          inner join role on manager_id = employee.manager_id 
+                          inner join department on manager_id = role.manager_id
+                        WHERE ?`, { name: response.managerName }, (error, results) => {
+          console.log("\n");
+          // DISPLAY results for user
+          console.table(results);
+          console.log("\n");
+          promptUser();
+        });
+      });
+    }
+  });
+}
+
+
+
+// FUNCTION TO VIEW ALL ROLES
+function viewAllRoles() {
+  // JOIN role AND department TABLES TO COMBINE DATA AND RETURN role ID, title, and department name DATA for the role chosen by user
+  connection.query(`SELECT role.id, role.title, department.name AS department, role.salary FROM role 
+                          INNER JOIN department ON department.id = role.department_id`, function (error, results) {
+    if (error) {
+      console.log(error);
+      connection.end();
+    } else {
+      console.log("\n");
+      // DISPLAY results for user
+      console.table(results);
+      console.log("\n");
+      promptUser();
+    }
+  });
+}
+
+
+// FUNCTION TO ADD new DEPARTMENT
+function addDepartment() {
+  // PROMPT USER FOR name OF NEW DEPARTMENT
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "What department would you like to add?",
+      name: "newDepartmentName",
+    },
+  ]).then((response) => {
+    // INSERT NEW department into department table
+    connection.query(`INSERT INTO department SET ?`, { name: response.newDepartmentName }, (error, results) => {
+      console.log("\n");
+      console.log(`Added ${response.newDepartmentName} to the DEPARTMENT database`)
+      console.log("\n");
+      promptUser();
+    });
+  });
+}
+
+
+// FUNCTION TO ADD new ROLE
+function addRole() {
+  // SELECT ALL FROM department; we will need to ACCESS the results(listOfDepartments) of the callback function
+  connection.query("SELECT * FROM department", (error, listOfDepartments) => {
+    if (error) {
+      console.log(error);
+      connection.end();
+    } else {
+      // create an array of the department names
+      const departmentNames = listOfDepartments.map((department) => department.name);
+
+      inquirer.prompt([
+        // prompt user for department that will house new role
+        {
+          type: "list",
+          message: "To which department should the new role belong?",
+          choices: departmentNames,
+          name: "departmentName"
+        },
+        // prompt user for title of new role
+        {
+          type: "input",
+          message: "What is the title for this role?",
+          name: "newRoleTitle",
+        },
+        // prompt user for salary of new role
+        {
+          type: "input",
+          message: "What is the salary for this role?",
+          name: "newRoleSalary",
+        },
+      ]).then((response) => {
+        //find department_id that matches user's department choice
+        const department = listOfDepartments.find((department) => department.name == response.departmentName);
+
+        // INSERT NEW role into role table
+        connection.query(`INSERT INTO role SET ?`, { title: response.newRoleTitle, salary: response.newRoleSalary, department_id: department.id }, (error, results) => {
+          console.log("\n");
+          console.log(`Added ${response.newRoleTitle} to the ${department.name} department`)
+          console.log("\n");
+          promptUser();
+        });
+      });
+    }
+  });
+}
+
+// FUNCTION TO ADD new EMPLOYEE
+function addEmployee() {
+  // SELECT ALL FROM role; we will need to ACCESS the results(listOfRoles) of the callback function
+  connection.query("SELECT * FROM role", (error, listOfRoles) => {
+    if (error) {
+      console.log(error);
+      connection.end();
+    } else {
+      // create an array of the role titles
+      const roleTitles = listOfRoles.map((role) => role.title);
+
+      inquirer.prompt([
+        // prompt user for employee first name
+        {
+          type: "input",
+          message: "Enter the employee's first name",
+          name: "employeeFirst"
+        },
+        // prompt user for employee last name
+        {
+          type: "input",
+          message: "Enter the employee's last name",
+          name: "employeeLast"
+        },
+        // prompt user for employee title
+        {
+          type: "list",
+          message: "What is the title for this employee?",
+          choices: roleTitles,
+          name: "employeeRole",
+        },
+      ]).then((response) => {
+        //find role that matches user's title choice
+        const role = listOfRoles.find((role) => role.title == response.employeeRole);
+        // INSERT NEW employee into employee table
+        connection.query(`INSERT INTO employee SET ?`, { first_name: response.employeeFirst, last_name: response.employeeLast, role_id: role.id }, (error, results) => {
+          console.log("\n");
+          console.log(`Added ${response.employeeFirst} ${response.employeeLast} to the database`)
+          console.log("\n");
+          promptUser();
+        });
+      });
+    }
+  });
+}
+
+function updateRole() {
+  connection.query(`SELECT employee.id, employee.first_name, employee.last_name, role_id, role.title, department.name AS department FROM employee	
+                      INNER JOIN role ON role.id = employee.role_id 
+                      INNER JOIN department ON department.id = role.department_id;`, function (error, results) {
+    if (error) {
+      console.log(error);
+      connection.end();
+    } else {
+      // DISPLAY results for user
+      console.table(results);
+
+      inquirer.prompt([
+        {
+          type: "input",
+          message: "Enter id number for the employee you would like to update.",
+          name: "employeeID"
+        },
+        {
+          type: "input",
+          message: "Enter role_id number for the employee's NEW role",
+          name: "roleID"
+        },
+      ]).then((response) => {
+        connection.query(`UPDATE employee SET role_id = ? WHERE id = ?`, [response.roleID, response.employeeID], function (error, results) {
+          if (error) {
+            console.log(error);
+            connection.end();
+          } else {
+            console.log("\n");
+            console.log("The employee role has been updated");
+            console.log("\n");
+            promptUser();
+          }
+        })
+      })
+    }
+  });
+}
+
+function updateEmployeeManager() {
+  connection.query(`SELECT employee.id, employee.first_name, employee.last_name, role_id, role.title, department.name AS department FROM employee	
+                      INNER JOIN role ON role.id = employee.role_id 
+                      INNER JOIN department ON department.id = role.department_id;`, function (error, results) {
+    if (error) {
+      console.log(error);
+      connection.end();
+    } else {
+      // DISPLAY results for user
+      console.table(results);
+
+      inquirer.prompt([
+        {
+          type: "input",
+          message: "Enter id number for the employee you would like to update.",
+          name: "employeeID"
+        },
+        {
+          type: "input",
+          message: "Enter role_id number for the employee's NEW role",
+          name: "roleID"
+        },
+      ]).then((response) => {
+        connection.query(`UPDATE employee SET role_id = ? WHERE id = ?`, [response.roleID, response.employeeID], function (error, results) {
+          if (error) {
+            console.log(error);
+            connection.end();
+          } else {
+            console.log("\n");
+            console.log("The employee role has been updated");
+            console.log("\n");
+            promptUser();
+          }
+        })
+      })
+    }
+  });
+
+}
+
+// FUNCTION TO DELETE EMPLOYEE
+function deleteEmployee() {
+  // SELECT employee ID number, first and lasdt names from employee table
+  connection.query(`SELECT employee.id, employee.first_name, employee.last_name FROM employee`, function (error, results) {
+    if (error) {
+      console.log(error);
+      connection.end();
+    } else {
+      // DISPLAY results for user
+      console.table(results);
+
+      inquirer.prompt([
+        // prompt user for ID number of employee to delete
+        {
+          type: "input",
+          message: "Enter id number for the employee you would like to delete.",
+          name: "employeeID"
+        },
+      ]).then((response) => {
+        // DELETE employee FROM employee table
+        connection.query(`DELETE FROM employee WHERE ?`, { id: response.employeeID }, function (error, results) {
+          if (error) {
+            console.log(error);
+            connection.end();
+          } else {
+            console.log("\n");
+            console.log(`Deleted employee from the database`);
+            console.log("\n");
+            promptUser();
+          }
+        })
+      })
+    }
+  });
+}
+
+
 
